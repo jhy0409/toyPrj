72ce778d39d171241dd88aa16c99f5440a7a9202
jhy0409 <inooph@naver.com>
2023-08-30 10:59:19 +0900

    alrtStyle - csSfUi ( 1. 테이블뷰, 테이블뷰 셀, 2. 뷰 나타난 뒤 dim animation, 3. 뷰 비율 )

--- CustomAlert/CustomAlert/CsVCs/CsSfUiVC.swift
+++ CustomAlert/CustomAlert/CsVCs/CsSfUiVC.swift
@@ -8,7 +8,15 @@
 import SwiftUI
 import UIKit
 
-struct CsSfUiVC: View {
+struct CsSfUiVC: View, PrBtnLayout {
+    var isDefPair: btnLayout = .withinZroIdx
+    
+    var btnTitleArr: [String] = []
+    var btnTitArr: [btnTitle] = []
+    
+    var titMsgViewFull: CGFloat = 1.0
+    var titMsgViewMax: CGFloat  = 0.6
+    
     @Environment(\.dismiss) var dismiss
     
     @State var didAppear: Bool = false {
@@ -19,34 +27,57 @@ struct CsSfUiVC: View {
         didAppear ? .primary.opacity(0.35) : .clear
     }
     
-    @State private var theCol: Color = .clear
+    var artTp: (title: String, msg: String) = ("title title title ", "desc desc desc")
+    
+    var tblMxHeight: CGFloat {
+        return viewSize.height * (1.0 - titMsgViewMax)
+    }
+    
+    var totalCellHgt: CGFloat {
+        return CGFloat(btnTitArr.count) * defCellHgt
+    }
+    
     
     var body: some View {
         
         ZStackLayout(alignment: Alignment(horizontal: .trailing, vertical: .top)) {
-            VStack(spacing: 25) {
-                Text("Congr")
-                    .fontWeight(.bold)
-                    .foregroundColor(.pink)
+            VStack(spacing: 16) {
                 
-                Button(action: {}) {
-                    Text("Back to Home")
+                ScrollView(.vertical, showsIndicators: true) {
+                    Text("\(artTp.title)")
+                        .fontWeight(.bold)
+                        .foregroundColor(.pink)
+                        .multilineTextAlignment(.center)
+                        .padding(.top, 40)
+                    
+                    Text("\(artTp.msg)")
                         .foregroundColor(.black)
                         .fontWeight(.regular)
                         .padding(.vertical, 10)
                         .padding(.horizontal, 25)
+                }.frame(maxHeight: viewSize.height * ( totalCellHgt > 0  ? titMsgViewMax : 0.2) )
+                
+                List(btnTitArr) { item in
+                    rowView(str: String(describing: item.idx))
                 }
+                // MARK: - [🔴] todo /// gdlk49
+                .frame(height: viewSize.height - titMsgViewMax - totalCellHgt > 0 ? tblMxHeight : totalCellHgt)
+                .background(.red)
+                .listStyle(.plain)
+                
             }
+            .frame(width: (viewSize.width * 0.65))
             .padding(.vertical, 25)
             .padding(.horizontal, 30)
             .background(BlurView())
             .cornerRadius(25)
             
-            
             Button(action: {
                 withAnimation {
                     didAppear.toggle()
-                    dismiss()
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                        dismiss()
+                    }
                 }
                 
             }) {
@@ -70,6 +101,21 @@ struct CsSfUiVC: View {
     
 }
 
+struct rowView: View {
+    var str: String
+    var body: some View {
+        HStack(alignment: .center) {
+            Button(str) {
+                print("--> tapped \(str)")
+            }.frame(maxWidth: .infinity)
+            
+            Button(str) {
+                print("--> tapped \(str)")
+            }.frame(maxWidth: .infinity)
+        }
+    }
+}
+
 struct BlurView: UIViewRepresentable {
     func makeUIView(context: Context) -> UIVisualEffectView {
         let view = UIVisualEffectView(effect: UIBlurEffect(style: .systemUltraThinMaterial))
@@ -85,5 +131,16 @@ struct BlurView: UIViewRepresentable {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         CsSfUiVC()
+    }
+}
+
+struct btnTitle: Identifiable {
+    var id = UUID()
+    var idx: String
+}
+
+extension View {
+    var viewSize: CGSize {
+        return .init(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
     }
 }
